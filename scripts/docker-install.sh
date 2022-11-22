9e3cdb9c12789709476414a88c7eed6427784338
david <iecanfly@gmail.com>
2022-11-22 09:11:19 +0800

    file name changed

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -16,7 +16,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
